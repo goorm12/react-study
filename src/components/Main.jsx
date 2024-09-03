d3400346b0325c4367f8f5d70428dba8186a0df9
binwoonki <vinwoonki98@gmail.com>
2024-09-03 21:17:01 +0900

    if문과 삼항연산자를 이용하여 컴포넌트 생성해보기

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,15 @@
 const Main = () => {
-  return (
-    <div>
-      <h1>main</h1>
-    </div>
-  );
+  const user = {
+    name: "구름",
+    isLogin: false,
+  };
+
+  if (user.isLogin) {
+    return <div>로그아웃</div>;
+  } else {
+    return <div>로그인</div>;
+  }
+  // return <>{user.isLogin ? <div>로그아웃</div> : <div>로그인</div>}</>;
 };
 
 export default Main;
