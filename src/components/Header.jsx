3de79859ffe060293bc61204626a501ebb10dbd6
binwoonki <vinwoonki98@gmail.com>
2024-09-05 16:47:52 +0900

    React.memo 를 사용하여 불필요한 렌더링 제어

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import "./Header.css";
+import { memo } from "react";
 const Header = () => {
   return (
     <div className="Header">
@@ -8,4 +9,4 @@ const Header = () => {
   );
 };
 
-export default Header;
+export default memo(Header);
