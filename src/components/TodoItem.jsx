ed71311bf7af60ffa35c5c973bb0dc3f9487f069
binwoonki <vinwoonki98@gmail.com>
2024-09-05 17:15:25 +0900

    useCallback을 사용한 최적화 1. 기능을 구현하고 2. 최적화를 하자 마지막에 최적화를 하는게 좋다.

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -21,14 +21,17 @@ const TodoItem = ({ id, isDone, content, date, onUpdate, onDelete }) => {
 
 // 컴포넌트를 인수로 받아서 해당 컴포넌트에 최적화나 메모이제이션 같은 주가적인 기능을 덧붙여서 기능이 추가된 새로운 컴포넌트를
 // 고차 컴포넌트(HOC)
-export default memo(TodoItem, (prevProps, nextProps) => {
-  // 반환값에 따라, Props가 바뀌었는지 안바뀌었는지 판단
-  // T -> Props 바뀌지 않음 -> 리렌더링 X
-  // F -> Props 바뀜 -> 리렌더링 O
-  if (prevProps.id !== nextProps.id) return false;
-  if (prevProps.isDone !== nextProps.isDone) return false;
-  if (prevProps.content !== nextProps.content) return false;
-  if (prevProps.date !== nextProps.date) return false;
+// export default memo(TodoItem, (prevProps, nextProps) => {
+//   // 반환값에 따라, Props가 바뀌었는지 안바뀌었는지 판단
+//   // T -> Props 바뀌지 않음 -> 리렌더링 X
+//   // F -> Props 바뀜 -> 리렌더링 O
+//   if (prevProps.id !== nextProps.id) return false;
+//   if (prevProps.isDone !== nextProps.isDone) return false;
+//   if (prevProps.content !== nextProps.content) return false;
+//   if (prevProps.date !== nextProps.date) return false;
 
-  return true;
-});
+//   return true;
+// });
+
+// useCallback
+export default memo(TodoItem);
